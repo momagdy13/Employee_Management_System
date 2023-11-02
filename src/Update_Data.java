cc6a55f156313cf7b4cbbe62da1bfed9044ccb04
MoMagdy <hamomagdy12266@gmail.com>
2023-11-02 15:57:46 +0200

    I'm Create Update_Data Class

---
+++ src/Update_Data.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Update_Data {
+    private JPanel update;
+    private JTextField name;
+    private JButton updateButton;
+    private JButton backButton;
+    private JTextField id;
+    private JButton searchh;
+    private JTextField father;
+    private JTextField date;
+    private JTextField addresss;
+    private JTextField phone;
+    private JTextField salary;
+    private JTextField email;
+    Connection connection = database_connection.connection();
+    Statement statement = null;
+
+    public Update_Data() {
+
+        JFrame frame = new JFrame("Update_Data");
+        frame.setContentPane(update);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(900, 700);
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new Home();
+            }
+        });
+        searchh.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    statement  = connection.createStatement();
+                    String ID = id.getText();
+                    String sql = "SELECT * FROM employees WHERE ID = '"+ID+"'";
+                    ResultSet rs = statement.executeQuery(sql);
+                    if (rs.next()){
+                        name.setText(rs.getString(2));
+                        father.setText(rs.getString(3));
+                        date.setText(rs.getString(4));
+                        salary.setText(rs.getString(5));
+                        addresss.setText(rs.getString(6));
+                        phone.setText(rs.getString(7));
+                        email.setText(rs.getString(8));
+                    }
+                }catch(Exception exception){
+                    JOptionPane.showMessageDialog(null,exception);
+                }
+            }
+        });
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    statement = connection.createStatement();
+                    String Name = name.getText();
+                    String Father = father.getText();
+                    String Phone = phone.getText();
+                    String Email = email.getText();
+                    String Address = addresss.getText();
+                    String Salary = salary.getText();
+                    String Date = date.getText();
+                    String sql = "UPDATE employees SET employee_name = '"+Name+"',father_name = '"+Father+"',DateOFBirth = '"+Date+"',salary = '"+Salary+"',address = '"+Address+"',phone = '"+Phone+"',Email_ID = '"+Email+"' WHERE ID = '"+id.getText()+"' ";
+                    statement.executeUpdate(sql);
+                    JOptionPane.showMessageDialog(null,"Data Update Successfully..!");
+                    clear();
+                }catch (Exception exception){
+                    JOptionPane.showMessageDialog(null,exception);
+                }
+            }
+        });
+    }
+
+    public void clear(){
+        name.setText("");
+        father.setText("");
+        phone.setText("");
+        addresss.setText("");
+        email.setText("");
+        salary.setText("");
+        date.setText("");
+    }
+}
